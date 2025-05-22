c2ec8a2bdba03a286cd6ee42eb886f2283588fa7
Alexandre Furquim <alefurquim@gmail.com>
2025-05-21 21:29:38 -0300

    DARTHVADERBOT.py

--- DARTHVADERBOT.py
+++ DARTHVADERBOT.py
@@ -92,7 +92,7 @@ def call_agent(agent: Agent, message_text: str) -> str:
 
 
 def agente_explorador(topico: str, data_de_hoje: str) -> str:
-    MODEL_NAME = "gemini-1.5-flash-latest" # Usando um modelo mais recente e geralmente disponível
+    MODEL_NAME = "gemini-2.5-flash-preview-05-20" # Usando um modelo mais recente e geralmente disponível
 
     explorador = Agent(
         name="explorador_vader",
